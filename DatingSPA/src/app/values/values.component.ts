2ab153bb287d5d2539dd19465f25bbd315543fbd
Parthabsl <partha.sarkar@birlasoft.com>
2019-10-06 12:32:58 +0530

    Initial Commit

---
+++ DatingSPA/src/app/values/values.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-values',
+  templateUrl: './values.component.html',
+  styleUrls: ['./values.component.css']
+})
+export class ValuesComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+values: any;
+  getValues(){
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+this.values = response;
+    }, error => {console.log(error);});
+  }
+
+}
